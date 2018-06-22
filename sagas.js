7aea435027d855ba2f9174c319e8e5ea981ba42b
Chaim Halbert <chalbert@evernote.com>
2018-06-22 09:14:07 -0700

    completed tutorial

---
+++ sagas.js
@@ -0,0 +1,24 @@
+import { delay } from 'redux-saga'
+import { put, takeEvery, all, call } from 'redux-saga/effects'
+
+function* helloSaga() {
+  console.log('Hello Sagas!')
+}
+
+// Our worker Saga: will perform the async increment task
+export function* incrementAsync() {
+  yield call(delay, 1000)
+  yield put({ type: 'INCREMENT' })
+}
+
+function* watchIncrementAsync() {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+export default function* rootSaga() {
+  yield all([
+    helloSaga(),
+    watchIncrementAsync()
+  ])
+}
+
